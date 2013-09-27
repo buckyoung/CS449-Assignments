1e43718e75d7001991cddce81b16dbee0836ca3e
professir <buckcyoung@gmail.com>
2013-09-27 19:09:42 -0400

    Completed Verify

--- Assignment1/exifview.c
+++ Assignment1/exifview.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-
-//Prototypes
-
-//Declarations
+#include <string.h>
 
 //Structs
 struct header {
@@ -11,7 +8,7 @@ struct header {
 	unsigned char app1_block_length[2];
 	unsigned char exif_string[4];
 	unsigned char null_zero[2];
-	unsigned char endianess[2];
+	unsigned char endianness[2];
 	unsigned char version_42[2];
 	unsigned char exif_block_offset[4];
 };
@@ -23,11 +20,12 @@ struct tiff_tag {
 	char value_or_offset[4];
 };
 
-int main(int argc, char argv[]){
+//Prototypes
+int verify(struct header*);
 
-	//Declarations
-	FILE* f;
-	struct header exif_head;
+//Declarations
+
+int main(int argc, char argv[]){
 
 	//Check for a single command-line argument
 	if (argc != 2){
@@ -35,22 +33,23 @@ int main(int argc, char argv[]){
 		return -1;
 	}
 
+	//Declarations
+	FILE* f;
+	struct header exif_head;
+
 	//Usage
-	
-	f = fopen("img1.jpg", "rb"); //open file to read binary
+	f = fopen("img1.jpg", "rb"); //open file to read binary //TODO (SegFault on argv[1]?!)
 
 
 	if (fread(&exif_head, sizeof(struct header), 1, f) == 1){ //Successfully read in exif/tiff header struct
 
 		//Verify JPG and APP1
-		if ( !(exif_head.jpg_start_of_file[0] == 0xFF && exif_head.jpg_start_of_file[1] == 0xD8) ){ //Verify this is a jpg
-			printf("Error: Cannot verify file as JPG");
+		if (!verify(&exif_head)){ //returns 0 if failed
+			printf("Exiting...\n");
 			return -3;
-		}
-		if ( !(exif_head.jpg_app1_marker[0] == 0xFF && exif_head.jpg_app1_marker[1] == 0xE1) ){ //Verify this is a APP1
-			printf("Error: Cannot verify file as APP1");
-			return -4;
-		}
+		} 
+
+		//Verified file, now lets start reading the TIFFs
 
 
 	} else { //FAILED to read in a header struct
@@ -58,5 +57,44 @@ int main(int argc, char argv[]){
 		return -2;
 	}
 
+
+}
+
+/*
+	verify() accepts the address of the header struct and will verify that the header is in good order and verify the endianness
+	@returns: 0 if failed, !0 if success
+*/
+int verify(struct header* s){
+
+		char a[5];
+
+		//Verify this is a jpg
+		if ( !(s->jpg_start_of_file[0] == 0xFF && s->jpg_start_of_file[1] == 0xD8) ){
+			printf("Error: Cannot verify file as JPG\n");
+			return 0;
+		}
+
+		//Verify this is a APP1
+		if ( !(s->jpg_app1_marker[0] == 0xFF && s->jpg_app1_marker[1] == 0xE1) ){ 
+			printf("Error: Cannot verify file as APP1\n");
+			return 0;
+		}
+
+		//Verify EXIF string
+		strcpy(a, s->exif_string); //copy "EXIF" into a
+		a[4] = '\0'; //add null terminator 
+		if ( !strcmp(a, "EXIF") ){
+			printf("Error: Tag not found\n");
+			return 0;
+		}
+
+		//Verify Endianness
+		if ( !(s->endianness[0] == 'I' && s->endianness[1] == 'I')){
+			printf("Error: Endianness not supported");
+			return 0;
+		}
+
+		//else, everything is good!
+		return 1;
 
 }
