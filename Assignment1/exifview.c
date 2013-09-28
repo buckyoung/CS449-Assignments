0abb99f6290a2435127e1786dd4a683c9431e298
professir <buckcyoung@gmail.com>
2013-09-27 21:02:40 -0400

    Created parse-tiffs (and sub) function

--- Assignment1/exifview.c
+++ Assignment1/exifview.c
@@ -18,15 +18,16 @@ struct tiff_tag {
 	unsigned short tag_id;
 	unsigned short data_type;
 	unsigned int num;
-	unsigned int offset;
+	unsigned int offset; //(or data)
 };
 
 //Prototypes
 int verify(struct header*);
-void print_m_m(int, int, FILE*);
+void print_data(int, int, FILE*);
+void parse_tiffs(FILE*);
+void parse_tiffs_sub(FILE*);
 
 //Declarations
-
 int main(int argc, char argv[]){
 
 	//Check for a single command-line argument
@@ -39,10 +40,6 @@ int main(int argc, char argv[]){
 	//Declarations
 	FILE* f;
 	struct header exif_head;
-	unsigned short count;
-	int i;
-	struct tiff_tag tiff;
-
 
 	//Usage
 	f = fopen("img1.jpg", "rb"); //open file to read binary //TODO (SegFault on argv[1]?!)
@@ -56,34 +53,10 @@ int main(int argc, char argv[]){
 			return -3;
 		}
 
-		//Verified file, now get count:
-		fread(&count, sizeof(short), 1, f); //at byte 20-21
+		//Verified, now parse for tiffs
+		parse_tiffs(f);
 
-		//for each count, read in tiff tag
-		for (i = 0; i < count; i++){
-			//Where are the tags? starting at byte 22. Note: tags are 12 bytes long
-			//OFFSET => 22+(12*i)
 
-			fread(&tiff, sizeof(struct tiff_tag), 1, f);
-
-			if (tiff.tag_id == 0x010F){
-				//Manufacturer 
-				printf("%-16s", "Manufacturer: ");
-				print_m_m(tiff.num, tiff.offset, f); //Print the manufacturer string
-				printf("\n");
-
-			} else if (tiff.tag_id == 0x0110){
-				//Camera Model
-				printf("%-16s", "Model: ");
-				print_m_m(tiff.num, tiff.offset, f); //Print the camera model string
-				printf("\n");
-
-			} else if (tiff.tag_id == 0x8769){
-				//Exif sub-block address
-
-			}
-
-		}
 
 	} else { //FAILED to read in a header struct
 		printf("Failed to read a single header struct from file");
@@ -105,7 +78,7 @@ int verify(struct header* s){
 		char a[5];
 
 		//Verify this is a jpg
-		if ( !((*s).jpg_start_of_file == 0xD8FF) ){
+		if ( !(s->jpg_start_of_file == 0xD8FF) ){
 			printf("Error: Cannot verify file as JPG\n");
 			return 0;
 		}
@@ -132,14 +105,92 @@ int verify(struct header* s){
 
 		//else, everything is good!
 		return 1;
+}
 
+/*
+	This method will accept a file and parse it for count tiff tags
+	(specifically for the manufacturer and camera model tags)
+*/
+void parse_tiffs(FILE* f){
+	//declarations:
+	unsigned short count;
+	int i;
+	struct tiff_tag tiff;
+	
+	//Usage:
+	//get count
+	fread(&count, sizeof(short), 1, f); //at byte 20-21
+
+	//for each count, read in tiff tag
+	for (i = 0; i < count; i++){
+		//Where are the tags? starting at byte 22. Note: tags are 12 bytes long
+		//OFFSET => 22+(12*i)
+		fread(&tiff, sizeof(struct tiff_tag), 1, f);
+
+		if (tiff.tag_id == 0x010F){
+			//Manufacturer 
+			printf("%-16s", "Manufacturer: ");
+				print_data(tiff.num, tiff.offset, f); //Print the manufacturer string
+			printf("\n");
+
+		} else if (tiff.tag_id == 0x0110){
+			//Camera Model
+			printf("%-16s", "Model: ");
+			print_data(tiff.num, tiff.offset, f); //Print the camera model string
+			printf("\n");
+
+		} else if (tiff.tag_id == 0x8769){
+			//Exif sub-block address
+			//Change the file-pointer, and dont worry about changing it back
+			fseek(f, (tiff.offset+12), SEEK_SET);
+			parse_tiffs_sub(f);
+			break; //After we encounter this tag (8769), we can stop searching!
+	
+		}
+	}
+}
+
+void parse_tiffs_sub(FILE* f){
+	//declarations:
+	unsigned short count;
+	int i;
+	struct tiff_tag tiff;
+	
+	//Usage:
+	//get count
+	fread(&count, sizeof(short), 1, f); //at byte 20-21
+
+	//for each count, read in tiff tag
+	for (i = 0; i < count; i++){
+		//Where are the tags? starting at byte 22. Note: tags are 12 bytes long
+		//OFFSET => 22+(12*i)
+		fread(&tiff, sizeof(struct tiff_tag), 1, f);
+
+		if (tiff.tag_id == 0x010F){
+			//Manufacturer 
+			printf("%-16s", "Manufacturer: ");
+				print_data(tiff.num, tiff.offset, f); //Print the manufacturer string
+			printf("\n");
+
+		} else if (tiff.tag_id == 0x0110){
+			//Camera Model
+			printf("%-16s", "Model: ");
+			print_data(tiff.num, tiff.offset, f); //Print the camera model string
+			printf("\n");
+
+		} else if (tiff.tag_id == 0x8769){
+			//Exif sub-block address
+			parse_tiffs_sub(f);
+	
+		}
+	}
 }
 
 /*
 	print_m_m() prints n characters from offset (o+12) in file f
 	it responsibly and intelligently controls the file-pointer (that is, it retains position across a call to print_m_m)
 */
-void print_m_m(int n, int o, FILE* f){
+void print_data(int n, int o, FILE* f){
 
 	long long int fpointer = ftell(f); //Saves file-pointer
 	char* a = malloc(n * sizeof(char));//create space for n characters of size 1byte (yes, redundant)
